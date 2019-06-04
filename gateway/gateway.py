a9d9c2b866b1ebc4dcbe3a72823ba65e671226d8
hayam8 <hayam8@uw.edu>
2019-06-03 19:02:10 -0700

    updated idk whatever

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -2,7 +2,6 @@ import paho.mqtt.client as mqtt
 import random
 import threading
 import json
-import DHT22
 from datetime import datetime
 from pymongo import MongoClient
 
@@ -12,15 +11,16 @@ NEEDS TO BE IMPLEMENTED:
     Authentication of gateway in management system
     
 """
-#Gateway Name
-name = "Pi_Gateway"
 
+#Gateway Name
+__name = "Pi_Gateway"
+__gateway_port = 1883
 # MQTT settings for MQTT communication that takes place locally on gateway
-__broker = ""
+__gateway_broker = ""
 
 # MQTT Settings for CloudMQTT broker working in cloud
-__MQTT_Broker = "postman.cloudmqtt.com"
-__MQTT_Port = 18467
+__cloud_broker = "postman.cloudmqtt.com"
+__cloud_port = 18467
 __username = ***REMOVED***
 __password = ***REMOVED***
 
@@ -29,20 +29,19 @@ __password = ***REMOVED***
 db = MongoClient(***REMOVED***)
 database = db['gateway']
 collection = database['DHT22']
-mqtt_topics = name + "/#" # all topics/devices under Pi_Gateway
-mqtt_post_topics = name + "/"
+mqtt_topics = __name + "/#" # all topics/devices under Pi_Gateway
+mqtt_post_topics = __name + "/"
 __Keep_Alive_Interval = 45
 #_MQTT_Topic = "Pi_Gateway/DHT22/Humidity"
 #_MQTT_Topic_Humidity = "Pi_Gateway/DHT22/Humidity"
 #_MQTT_Topic_Temperature = "Pi_Gateway/DHT22/Temperature"
-
 
 def on_connect(client, userdata, rc):
     if rc != 0:
         pass
         print("Unable to connect to MQTT Broker...")
     else:
-        print("Connected with MQTT Broker: " + str(__MQTT_Broker))
+        print("Connected with MQTT Broker: " + str(__cloud_broker))
         client.subscribe(mqtt_topics)
 
 def on_publish(client, userdata, mid):
@@ -71,44 +70,30 @@ def on_message(client, userdata, msg):
         post = {"time": receive_time, "topic": msg.topic, "value": val}
     else:
         print(str(receive_time) + ": " + msg.topic + " " + message)
-        post = {"time": receive_time, "topic": msg.topic, "value" : message}
+        post = {"time": receive_time, "topic": msg.topic, "value": message}
 
     collection.insert_one(post)
 
 
-# connecting to mqtt broker and setting commands
-client = mqtt.Client(name)
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_publish = on_publish
-client.on_message = on_message
-client.connect(__MQTT_Broker, int(__MQTT_Port), int(__Keep_Alive_Interval))
-client.username_pw_set(__username, __password) # must log in with username/pass for broker
+# connecting to MQTT Cloud broker and setting commands
+__cloud_client = mqtt.Client(__name)
+__cloud_client.on_connect = on_connect
+__cloud_client.on_disconnect = on_disconnect
+__cloud_client.on_publish = on_publish
+__cloud_client.on_message = on_message
+__cloud_client.connect(__cloud_broker, int(__cloud_port), int(__Keep_Alive_Interval))
+__cloud_client.username_pw_set(__username, __password)  # must log in with username/pass for broker
 
 
-def publish_to_topic(topic, message):
-    client.publish(topic, message)
-    print("Published: " + str(message) + " " + "on MQTT Topic: " + str(topic))
-    print("\n")
+# connecting to MQTT Cloud broker and setting commands
+__client = mqtt.Client(__name)
+__client.on_connect = on_connect
+__client.on_disconnect = on_disconnect
+__client.on_publish = on_publish
+__client.on_message = on_message
+__client.connect(__name, int(__gateway_port), int(__Keep_Alive_Interval))
 
+__client.loop_forever()
 
-# FAKE SENSOR
-# Dummy code used as Fake Sensor to publish some random values to MQTT Broker
-
-
-def publish_fake_sensor_values_to_mqtt():
-    # threading.Timer(3.0, publish_fake_sensor_values_to_mqtt).start()
-    # these values are specific to the fake sensor data being collected
-    device = DHT22.get_device()
-    # checks if device name is in local data storage, doesn't collect information otherwise
-    if device in database.list_collection_names():
-        sensor_type = DHT22.get_sensor_type()
-        sensor_data = DHT22.fake_sensor_values()
-        topic = name + "/" + device + "/" + sensor_type
-        publish_to_topic(topic, sensor_data)
-
-
-publish_fake_sensor_values_to_mqtt()
-
-client.loop_forever()
+__cloud_client.loop_forever()
 # ====================================================
